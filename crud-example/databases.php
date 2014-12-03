0b276a344412c3b0bfca34e6ba438c9cc519586f
Ingrid <Kelder>
2014-12-03 12:06:07 +0200

    form

--- crud-example/databases.php
+++ crud-example/databases.php
@@ -5,20 +5,12 @@ $dbpass = "psroolike";
 $dbname = "widget_corp";
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-
 if (mysqli_connect_errno()) {
   // Juhul kui veateate kood on olemas, teosta siin plokis paiknevad tegevused.
-  die("Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ").");
+  die("Database connection failed: ") . mysqli_connect_error() . " (" . mysqli_connect_errno() . ").";
 }
 
-if(isset($_POST['submit'])){
-$menu_name = $_POST['menu_name'];
-$position = $_POST['position'];
-$visible = $_POST['visible'];
-$query = "INSERT INTO subjects (menu_name, position, visible)
-            VALUES ('$menu_name', '$position', '$visible')";
-}
-
+$query = "SELECT * FROM subjects";
 $result = mysqli_query($connect, $query);
 ?>
 <!DOCTYPE HTML>
@@ -28,10 +20,10 @@ $result = mysqli_query($connect, $query);
 </head>
 <body>
 <?php
-/* while ($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
     echo "<ul><li>" . $row['menu_name'] . "</li></ul>";
 }
-/* $query1 = "SELECT * FROM pages ORDER BY menu_name";
+$query1 = "SELECT * FROM pages ORDER BY menu_name";
 $result1 = mysqli_query($connect, $query1);
 echo "<br><hr><br>";
 while ($row1 = mysqli_fetch_assoc($result1)) {
@@ -51,36 +43,11 @@ while ($row2 = mysqli_fetch_assoc($result2)) {
     echo $row2['content'];
     echo '</div></article>';
 }
-echo "<br><hr><br>"; */
+echo "<br><hr><br>";
 ?>
-<?php
-  if ($result) {
-    echo "Õnnestus";
-  } else {
-    echo "Ebaõnnestus";
-  }
-?>
-
-<?php
-if(isset($_POST['submit'])){
-echo $answer;
-}
-?>
-
-
-<form action="database-create.php" method="post">
-	<input type="text" name="menu-name" placeholder="menu name">
-	<br>
-	<input name="position" type="text" placeholder="position">
-	<br>
-	<input name="visible" type="text" placeholder="Visible">
-	<br>
-	<button type="submit" value="submit">Submit</button>
-</form>
-
 </body>
 </html>
- <?php
-/* mysqli_free_result($result);
-mysqli_close($connect); */
+<?php
+mysqli_free_result($result);
+mysqli_close($connect);
 ?>
